550ced27a4dedc578804c1d8fa4f77ae82e7bd43
caseBread <kgu0515@gmail.com>
2023-02-25 22:57:33 +0900

    fix : eslint 버그 수정

--- frontend/src/sections/Footer.tsx
+++ frontend/src/sections/Footer.tsx
@@ -1,5 +1,5 @@
-function Footer() {
-  return <div style={{ border: 'solid 1px blue', width: '375px' }}>Footer</div>;
-}
+const Footer = () => {
+  return <div style={{ border: "solid 1px blue", width: "375px" }}>Footer</div>;
+};
 
 export default Footer;
--- frontend/src/sections/LayoutTool.tsx
+++ frontend/src/sections/LayoutTool.tsx
@@ -1,7 +1,5 @@
 function LayoutTool() {
-  return (
-    <div style={{ border: 'solid 1px blue', width: '375px' }}>LayoutTool</div>
-  );
+  return <div style={{ border: "solid 1px blue", width: "375px" }}>LayoutTool</div>;
 }
 
 export default LayoutTool;
