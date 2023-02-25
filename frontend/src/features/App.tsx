c0f0ef46f72ab9ed93e0f8e81952e56a59315b5f
caseBread <kgu0515@gmail.com>
2023-02-25 21:05:18 +0900

    fix : eslint prettier 충돌 오류 해결

--- frontend/src/features/App.tsx
+++ frontend/src/features/App.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../app/hooks';
-import InteractiveContainer from '../components/InteractiveContainer';
-import BackgroundTool from '../sections/BackgroundTool';
-import Footer from '../sections/Footer';
-import Header from '../sections/Header';
-import LayoutTool from '../sections/LayoutTool';
-import Preview from '../sections/Preview';
-import TextTool from '../sections/TextTool';
-import { helloWorld, selectHello } from './appSlice';
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import InteractiveContainer from "../components/InteractiveContainer";
+import BackgroundTool from "../sections/BackgroundTool";
+import Footer from "../sections/Footer";
+import Header from "../sections/Header";
+import LayoutTool from "../sections/LayoutTool";
+import Preview from "../sections/Preview";
+import TextTool from "../sections/TextTool";
+import { helloWorld, selectHello } from "./appSlice";
 
 const App = () => {
   const dispatch = useAppDispatch();
@@ -17,7 +17,7 @@ const App = () => {
     dispatch(helloWorld());
   }, []);
 
-return (
+  return (
     <InteractiveContainer>
       <Header />
       <Preview />
