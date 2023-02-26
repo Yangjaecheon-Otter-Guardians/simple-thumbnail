c3b2aa5b3ef8559a4c7c5dfc63fe3ddbe803d424
anveloper <hitedin@gmail.com>
2023-02-26 16:35:33 +0900

    refactor: 불필요 함수 및 레이아웃 컴포넌트 제거

--- frontend/src/features/App.tsx
+++ frontend/src/features/App.tsx
@@ -1,29 +1,17 @@
-import React, { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../app/hooks';
 import InteractiveContainer from '../components/InteractiveContainer';
 import BackgroundTool from '../sections/BackgroundTool';
 import Footer from '../sections/Footer';
 import Header from '../sections/Header';
-import LayoutTool from '../sections/LayoutTool';
 import Preview from '../sections/Preview';
 import TextTool from '../sections/TextTool';
-import { helloWorld, selectHello } from './appSlice';
 
 const App = () => {
-  const dispatch = useAppDispatch();
-  const hello = useAppSelector(selectHello);
-
-  useEffect(() => {
-    dispatch(helloWorld());
-  }, []);
-
   return (
     <>
       <Header />
       <InteractiveContainer>
         <Preview />
         <BackgroundTool />
-        <LayoutTool />
         <TextTool />
       </InteractiveContainer>
       <Footer />
