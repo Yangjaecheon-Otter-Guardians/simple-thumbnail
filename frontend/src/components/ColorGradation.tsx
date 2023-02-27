c3697266ab6591f8e774ee18e7b8757194b32828
headring <myhakiki@gmail.com>
2023-02-27 14:36:19 +0900

    refactor: 컬러픽커에서 랜덤 그라데이션으로 변경

--- frontend/src/components/ColorGradation.tsx
+++ frontend/src/components/ColorGradation.tsx
@@ -1,24 +1,23 @@
 import { useState } from 'react';
 import { BsCheck } from 'react-icons/bs';
-import { ChromePicker } from 'react-color';
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 import { isImageBright, previewColor, previewGradation } from 'atom';
 import { useEffect } from 'react';
+import { Icon } from '@iconify/react';
 
 const singleColorArray = [
-  [0, 'linear-gradient(180deg, #FF3737 0%, #FFDF37 100%)'],
-  [1, 'linear-gradient(180deg, #FF9549 0%, #AF7EFF 100%)'],
-  [2, 'linear-gradient(180deg, #FFE03C 0%, #3CFFA1 100%)'],
-  [3, 'linear-gradient(180deg, #8BFF43 0%, #FF855F 100%)'],
-  [4, 'linear-gradient(180deg, #7FE0FF 0%, #FFAD72 100%)'],
-  [5, 'linear-gradient(180deg, #D4B9FF 0%, #8C45FF 100%)'],
-  [6, 'linear-gradient(180deg, #FFFFFF 0%, #FFB6B6 100%)'],
+  [0, 'linear-gradient(134.01deg, #FFD9D9 9.35%, #FF2B2B 100%)'],
+  [1, 'linear-gradient(135deg, #87FAA9 0.01%, #BB86DD 100%)'],
+  [2, 'linear-gradient(137.82deg, #F8E92D 4.46%, #33E6F8 100%)'],
+  [3, 'linear-gradient(132.18deg, #FC941F 0%, #7836F8 95.54%)'],
+  [4, 'linear-gradient(135deg, #7EFBFE 0%, #2C2EDE 100%)'],
+  [5, 'linear-gradient(135deg, #FF79D1 0%, #FFCE33 100%)'],
+  [6, 'linear-gradient(135deg, #3FFA4B 0%, #453EF7 100%)'],
 ];
 
 export const ColorGradation = () => {
-  const [currentGradation, setCurrentGradation] = useRecoilState(previewGradation);
+  const setCurrentGradation = useSetRecoilState(previewGradation);
   const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [isPop, setIsPop] = useState(false);
   const setIsBright = useSetRecoilState(isImageBright);
   const setCurrentColor = useSetRecoilState(previewColor);
 
@@ -31,15 +30,24 @@ export const ColorGradation = () => {
       setCurrentIndex(pickedIndex);
     }
   };
-  const onChange = (color: string) => {
+
+  const hexString = '0123456789abcdef';
+  const randomColor = () => {
+    let hexCode = '#';
+    for (let i = 0; i < 6; i++) {
+      hexCode += hexString[Math.floor(Math.random() * hexString.length)];
+    }
+    return hexCode;
+  };
+  const generateRandom = () => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     if (currentIndex! >= 0) {
       setCurrentIndex(NaN);
     }
-    setCurrentGradation(color);
-  };
-  const pop = () => {
-    setIsPop((prev) => !prev);
+    const colorOne = randomColor();
+    const colorTwo = randomColor();
+    const angle = Math.floor(Math.random() * 360);
+    setCurrentGradation(`linear-gradient(${angle}deg, ${colorOne} 0%, ${colorTwo} 100%)`);
   };
 
   useEffect(() => {
@@ -61,19 +69,9 @@ export const ColorGradation = () => {
           {currentIndex === idx && <BsCheck style={{ color: 'black' }} />}
         </button>
       ))}
-      <button
-        className="w-8 h-8 rounded"
-        style={{
-          background:
-            'conic-gradient(from 180deg at 50% 50%, #FF5151 0deg, #FFF850 115.5deg, #50FFA1 218.62deg, #6950FF 360deg)',
-        }}
-        onClick={pop}
-      ></button>
-      {isPop && (
-        <div className="absolute z-10">
-          <ChromePicker color={currentGradation} onChange={(color) => onChange(color.hex)} />
-        </div>
-      )}
+      <button className="w-8 h-8 rounded border-2 flex justify-center items-center" onClick={generateRandom}>
+        <Icon icon="vaadin:random" vFlip={true} />
+      </button>
     </div>
   );
 };
