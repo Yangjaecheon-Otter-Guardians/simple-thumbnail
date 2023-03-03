40dfb0a3bf39e9feb4a8756aad67d0fa5643bc78
da-in <talown@naver.com>
2023-03-03 20:02:12 +0900

    :recycle: refactor : refactor divide line under preview
    # 113

--- frontend/src/features/App.tsx
+++ frontend/src/features/App.tsx
@@ -17,6 +17,7 @@ const App = () => {
     <>
       <Header />
       <Preview previewRef={previewRef} />
+      <DivideLine />
       <InteractiveContainer>
         <RatioTool />
         <BackgroundTool />
