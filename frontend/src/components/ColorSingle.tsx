6deaac9c6521d8e7f1f08ba6b171c80a3554cc8a
caseBread <kim.geonu@cashwalk.io>
2023-04-01 21:55:58 +0900

    refactor : 단색배경에 aria-label 추가

--- frontend/src/components/ColorSingle.tsx
+++ frontend/src/components/ColorSingle.tsx
@@ -5,14 +5,21 @@ import { BsCheck } from 'react-icons/bs';
 import { IoMdClose } from 'react-icons/io';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 
-const singleColorArray = [
-  [0, '#ff3737', 'bg-backRed', '빨강'],
-  [1, '#ff9549', 'bg-backOrange', '주황'],
-  [2, '#ffe03c', 'bg-backYellow', '노랑'],
-  [3, '#4fcf00', 'bg-backGreen', '초록'],
-  [4, '#30cdff', 'bg-backBlue', '하늘'],
-  [5, '#0E0E0E', 'bg-primary-100', '검정'],
-  [6, '#ffffff', 'bg-bakcWhite', '하양'],
+interface ISingleColorData {
+  index: string;
+  color: string;
+  tailwind: string;
+  label: string;
+}
+
+const singleColorArray: ISingleColorData[] = [
+  { index: '0', color: '#ff3737', tailwind: 'bg-backRed', label: '빨강' },
+  { index: '1', color: '#ff9549', tailwind: 'bg-backOrange', label: '주황' },
+  { index: '2', color: '#ffe03c', tailwind: 'bg-backYellow', label: '노랑' },
+  { index: '3', color: '#4fcf00', tailwind: 'bg-backGreen', label: '초록' },
+  { index: '4', color: '#30cdff', tailwind: 'bg-backBlue', label: '하늘' },
+  { index: '5', color: '#0E0E0E', tailwind: 'bg-primary-100', label: '검정' },
+  { index: '6', color: '#ffffff', tailwind: 'bg-bakcWhite', label: '하양' },
 ];
 
 const ColorSingle = () => {
@@ -73,14 +80,14 @@ const ColorSingle = () => {
       {singleColorArray.map((color, idx) => (
         <button
           type="button"
-          id={String(color[0])}
-          key={idx}
-          value={color[1]}
+          id={color.index}
+          key={color.index}
+          value={color.color}
           className={`flex justify-center items-center rounded border w-8 h-8 tablet:w-[48px] tablet:h-[48px] ${
-            color[2]
-          } ${currentIndex === color[0] && 'border'}`}
+            color.tailwind
+          } ${String(currentIndex) === color.index && 'border'}`}
           onClick={(e) => onClick(e)}
-          aria-label={color[3]}
+          aria-label={color.label}
         >
           {currentIndex === idx && (
             <BsCheck className={'tablet:text-[32px]'} style={{ color: idx === 5 ? 'white' : 'black' }} />
